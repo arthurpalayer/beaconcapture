6cc72b0df0acc05c36a057cff85c14f6c3925efd
Arthur Palayer <apalayer@purdue.edu>
2024-04-10 18:17:58 -0400

    stuff

--- controller/finalcontrol.py
+++ controller/finalcontrol.py
@@ -1,15 +1,11 @@
-import os
 from numpy import uint64, uint32
 import time
 from gpiozero import Button, LED, MCP3002
 import socket
 import numpy as np
 import threading as thread
-from picamera2.outputs import FileOutput
-import io
 import cv2
 import pickle
-from picamera2 import Picamera2
 
 #######64 bit packets 47:38 x1, 37:28 y1, 27:18 x2, 17:8 y2, 7:5 sw, 4:0 buttons
 
@@ -42,32 +38,46 @@ SCLK = 11
 
 #HOST = "127.0.0.1"
 HOST = "10.42.0.1"
+HOSTSERV = "10.42.0.254"
 PORT1 = 6969
 PORT2 = 6968
+VIDBUFFSIZE = 1000000
 
-def startsock(portnum):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
 def setupcontrol():
     #copy paste control code here
-    global sw0, sw1, sw2, pb0, pb1, pb2, pb3, pb4, led0, led1, led2, x1, x2, y1, y2
+    global sw0 
     sw0 = Button(SW0, pull_up=None, active_state=True)
+    global sw1 
     sw1 = Button(SW1, pull_up=None, active_state=True)
+    global sw2 
     sw2 = Button(SW2, pull_up=None, active_state=True)
+    global pb0 
     pb0 = Button(PB0, pull_up=None, active_state=True)
+    global    pb1 
     pb1 = Button(PB1, pull_up=None, active_state=True)
+    global pb2 
     pb2 = Button(PB2, pull_up=None, active_state=True)
+    global pb3 
     pb3 = Button(PB3, pull_up=None, active_state=True)
+    global pb4 
     pb4 = Button(PB4, pull_up=None, active_state=True)
-    led0 = LED(LED0)
+    global led0
+    led0= LED(LED0)
+    global led1
     led1 = LED(LED1)
-    led2 = LED(LED2)
+    global led2
+    led2= LED(LED2)
 
+    global x1
     x1 = MCP3002(channel=0, clock_pin=SCLK, mosi_pin=MOSI, miso_pin=MISO, select_pin=CS0)
-    x2 = MCP3002(channel=0, clock_pin=SCLK, mosi_pin=MOSI, miso_pin=MISO, select_pin=CS1)
+    global x2
+    x2= MCP3002(channel=0, clock_pin=SCLK, mosi_pin=MOSI, miso_pin=MISO, select_pin=CS1)
+    global y1
     y1 = MCP3002(channel=1, clock_pin=SCLK, mosi_pin=MOSI, miso_pin=MISO, select_pin=CS0)
-    y2 = MCP3002(channel=1, clock_pin=SCLK, mosi_pin=MOSI, miso_pin=MISO, select_pin=CS1)
+    global y2
+    y2= MCP3002(channel=1, clock_pin=SCLK, mosi_pin=MOSI, miso_pin=MISO, select_pin=CS1)
 
 def controlcheck():
 
@@ -77,21 +87,14 @@ def controlcheck():
       # print("PACKETYAY")
         packet = packet | 32
         dataset.append("Switch 0: ON")
-        led0.on()
-    else:
-        led0.off()
     if(sw1.is_pressed):
         packet = packet | 64
         led1.on()
         dataset.append("SWITCH 1: ON")
-    else:
-        led1.off()
     if (sw2.is_pressed):
         dataset.append("SWITHC 2: ON")
         led2.on()
         packet = packet | 128 
-    else:
-        led2.off()
     if (pb0.is_pressed):
         dataset.append("PB0 : ON")
         packet = packet | 1
@@ -107,7 +110,6 @@ def controlcheck():
     if (pb4.is_pressed):
         dataset.append("PB4: ON")
         packet = packet | 16
-    print(dataset)
 
     dataset.clear()
     x1u = int(x1.value * 1024) #percentage of 1024, can decrease granularity here 
@@ -123,17 +125,18 @@ def controlcheck():
     y2u = y2u & bitmask
     packet = packet | (y2u << (Y2))
     packet = int(packet)
-    print(packet)
     return packet
 
-def control(timeset):
+def control():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     setupcontrol()
+    timeset = 0.005
     while (1):
         packet = controlcheck()
         #        packet = struct.pack('!I', aint) 
         packet = packet.to_bytes(16)
         led1.off()
-        s.sendto(packet, (HOST, PORT1))
+        s.sendto(packet, (HOST, PORT2))
         led1.on()
         time.sleep(timeset )
 
@@ -150,20 +153,25 @@ def lcd():
         draw.text((x,y), status, fill = "white", font = font)
         sleep(0.5)
 
-def videoserver():
-    s.bind((HOST, PORT1))
+
+def videosocket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((HOSTSERV, PORT1))
+    print("1")
     while 1:
         data, addr = s.recvfrom(VIDBUFFSIZE)
+        print(data)
         data = pickle.loads(data)
         data = cv2.imdecode(data, cv2.IMREAD_COLOR)
         cv2.imshow("YAGADGISDJBG", data)
         
+def donothing():
+    time.sleep(0.2)
 
 if __name__ == "__main__":
     global s
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    t1 = thread.Thread(target=control, args=0.005)
-    t2 = thread.Thread(target=video)
+    t1 = thread.Thread(target=donothing)
+    t2 = thread.Thread(target=videosocket)
     t1.start()
     t2.start()
     t1.join()
