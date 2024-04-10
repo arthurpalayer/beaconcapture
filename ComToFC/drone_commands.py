c7757405b21b589fbb1291568e357946aed91669
arthur palayer <apalayer@purdue.edu>
2024-04-10 17:21:13 -0400

    added

--- ComToFC/drone_commands.py
+++ ComToFC/drone_commands.py
@@ -22,40 +22,55 @@ print("Flight controller Connected")
 time .sleep(1.0)
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind((HOST, PORT))
+	s.bind((HOST, PORT))
    
-    msg = "recvd" 
+	msg = "recvd" 
     
-    board.enable_arm()          #enable arming
-    board.arm()              #arm the board
+	board.enable_arm()          #enable arming
+	board.arm()              #arm the board
+	try: 
+		while 1:
+			data, addr = s.recvfrom(BUFFSIZE)
+			data = int.from_bytes(data)
+			converted_data = packetconvert(data)
+			#print(converted_data[0])
+			#print(converted_data[1])
+			rudder = converted_data[2] / 1024           #left x axis
+			throttle = converted_data[4] / 1024         #left y axis
+			aileron = converted_data[3] / 1024          #right x axis
+			elevator = converted_data[5] / 1024         #right y axis
 
-    while 1:
-	    data, addr = s.recvfrom(BUFFSIZE)
-	    data = int.from_bytes(data)
-	    converted_data = packetconvert(data)
-	    print(converted_data[0])
-	    print(converted_data[1])
-	    rudder = converted_data[2] / 1024           #left x axis
-	    throttle = converted_data[4] / 1024         #left y axis
-	    aileron = converted_data[3] / 1024          #right x axis
-	    elevator = converted_data[5] / 1024         #right y axis
+			print(rudder, throttle, elevator, aileron)
+			buf = []
+			push16(buf, int(aileron * 1000 + 1000))		    # aileron
+			push16(buf, int(elevator * 1000 + 1000))	    # elevator
+			push16(buf, int(throttle * 1000 + 1000))	    # throttle
+			push16(buf, int(rudder * 1000 + 1000))		    # rudder
+			push16(buf, 1500)		                        # aux1
+			push16(buf, 1000)		                        # aux2
+			push16(buf, 1000)		                        # aux3
+			push16(buf, 1000)		                        # aux4
+			board.sendCMD(MultiWii.SET_RAW_RC, buf)
 
-	    print(rudder, throttle, elevator, aileron)
-	    buf = []
-	    push16(buf, int(aileron * 1000 + 1000))		    # aileron
-	    push16(buf, int(elevator * 1000 + 1000))	    # elevator
-	    push16(buf, int(throttle * 1000 + 1000))	    # throttle
-	    push16(buf, int(rudder * 1000 + 1000))		    # rudder
-	    push16(buf, 1500)		                        # aux1
-	    push16(buf, 1000)		                        # aux2
-	    push16(buf, 1000)		                        # aux3
-	    push16(buf, 1000)		                        # aux4
-	    board.sendCMD(MultiWii.SET_RAW_RC, buf)
+			time.sleep(0.005)
+			if(converted_data[1][0] == 1):
+				break
 
-	    time.sleep(0.025)
-	    if(converted_data[1][0] == 1):
-	        break
+	except KeyboardInterrupt:
+		for x in range(20):
+			buf = [] 
+			push16(buf, 1000)
+			push16(buf, 1000)
+			push16(buf, 1000)
+			push16(buf, 1000)
+			push16(buf, 1000)
+			push16(buf, 1000)
+			push16(buf, 1000)
+			push16(buf, 1000)
+			board.sendCMD(MultiWii.SET_RAW_RC, buf)
+			time.sleep(0.025)
 
-    board.disarm()          #disarm the board
+		board.disarm()          #disarm the board
+		board.disable_arm()
     
          
