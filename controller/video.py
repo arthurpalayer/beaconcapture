c3abb981ce65cdc3622317bf5c75b33e3402efe6
Arthur Palayer <apalayer@purdue.edu>
2024-04-11 20:09:07 -0400

    working video once again W

--- controller/video.py
+++ controller/video.py
@@ -1,10 +1,11 @@
+import threading as thread
 from picamera2 import Picamera2
 import cv2
 import pickle
 import socket
-import time.sleep
+from time import sleep
 
-HOST = "10.42.0.1"
+HOST = '127.0.0.1'
 VIDPORT = 6967
 VIDBUFFSIZE = 100000
 PACKETSIZE = 8
@@ -16,7 +17,7 @@ def makeconn(s):
 def sendconn(s):
     msg = "conn" 
     msg = msg.encode('utf-8')
-    sendto(msg, (HOST, VIDPORT))
+    s.sendto(msg, (HOST, VIDPORT))
 
 def videoserver():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -29,8 +30,6 @@ def videoserver():
         ret, buffer = cv2.imencode(".jpg", im, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
         x = pickle.dumps(buffer)
         s.sendto(x, addr)
-        time.sleep(0.025)
-        print("1")
 
 def videorecv():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -40,4 +39,12 @@ def videorecv():
         data = pickle.loads(data)
         data = cv2.imdecode(data, cv2.IMREAD_COLOR)
         cv2.imshow("LIVEFEED", data)
-        sleep(0.05)
+
+
+if __name__ == "__main__":
+    t1 = thread.Thread(target=videoserver)
+    t2 = thread.Thread(target=videorecv)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
