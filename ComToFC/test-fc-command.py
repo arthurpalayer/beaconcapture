71713bbc6610df15017a645b1b38a07e5a275622
arthur palayer <apalayer@purdue.edu>
2024-04-13 16:27:19 -0400

    shit 3

--- ComToFC/test-fc-command.py
+++ ComToFC/test-fc-command.py
@@ -9,19 +9,21 @@ time.sleep(1.0)
 
 board.enable_arm()
 board.arm()
+try:
+    while True:
+        buf = []
+        push16(buf, 1500)		# aileron
+        push16(buf, 1500)		# elevator
+        push16(buf, 1500)		# throttle
+        push16(buf, 1500)		# rudder
+        push16(buf, 1500)		# aux1
+        push16(buf, 1000)		# aux2
+        push16(buf, 1000)		# aux3
+        push16(buf, 1000)		# aux4
+        board.sendCMD(MultiWii.SET_RAW_RC, buf)
+    
+        time.sleep(0.05)
+except KeyboardInterrupt:
+	board.disarm()
+	board.disable_arm()
 
-while True:
-    buf = []
-    push16(buf, 1500)		# aileron
-    push16(buf, 1500)		# elevator
-    push16(buf, 1500)		# throttle
-    push16(buf, 1500)		# rudder
-    push16(buf, 1500)		# aux1
-    push16(buf, 1000)		# aux2
-    push16(buf, 1000)		# aux3
-    push16(buf, 1000)		# aux4
-    board.sendCMD(MultiWii.SET_RAW_RC, buf)
-
-    #board.getData(MultiWii.RC)
-    #print(board.rcChannels)
-    time.sleep(0.05)
