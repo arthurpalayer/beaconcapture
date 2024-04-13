7e712cdf3d1ab28fc9e7c84e1f04fab5491c3530
arthur palayer <apalayer@purdue.edu>
2024-04-13 18:33:40 -0400

    testing flying

--- ComToFC/drone_commands.py
+++ ComToFC/drone_commands.py
@@ -22,7 +22,6 @@ def low_motor(board, speed):
 	push16(buf, 1000)
 	push16(buf, 1000)
 	board.sendCMD(MultiWii.SET_RAW_RC, buf)
-	time.sleep(0.025)
 
 def landing(board):
 	for x in range(20):
@@ -36,27 +35,50 @@ def landing(board):
 		push16(buf, 1000)
 		push16(buf, 1000)
 		board.sendCMD(MultiWii.SET_RAW_RC, buf)
-		time.sleep(0.025)
 
 def control():
 	board = MultiWii("/dev/ttyACM0")
-	time.sleep(1.0)
 	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-		s.bind((HOST, PORT))
+		flag = 1
+		while flag != 0:
+			try:
+				s.bind((HOST, PORT))
+				flag = 0
+				print("conection")
+			except: 
+				time.sleep(0.05)
+				print("no connection")
+				pass
 	   
-		msg = "recvd" 
-	    
+		msg = "conn established"
+		msgfall = "falling"
+		msguser = "user mode"
+		msgauto = "automode"
+		msgoff = "user off"	
+		msg = msg.encode('utf-8')
+		msgoff = msgoff.encode('utf-8')
+		msgfall = msgfall.encode('utf-8')
+		msgauto = msgauto.encode()
+		msguser = msguser.encode()
+
 		board.enable_arm()          #enable arming
 		board.arm()              #arm the board
+		
 		try: 
 			while 1:
 				data, addr = s.recvfrom(BUFFSIZE)
 				data = int.from_bytes(data)
+				s.sendto(msg, addr)
 				converted_data = packetconvert(data)
 				if(converted_data[0] != 0):
 					print("falling")
+				#	landing(board)
+					board.disarm()
+					board.disable_arm()
+					s.sendto(msgfall, addr)
 					break
 				elif converted_data[1][2]:
+					s.sendto(msguser, addr)
 					rudder = converted_data[2] / 1024           #left x axis
 					throttle = converted_data[4] / 1024         #left y axis
 					aileron = converted_data[3] / 1024          #right x axis
@@ -75,19 +97,16 @@ def control():
 					board.sendCMD(MultiWii.SET_RAW_RC, buf)
 
 				elif(not converted_data[1][2]):
+					s.sendto(msgoff, addr)
 					print("Manual control off")
 					low_motor(board, 1500)
 
-				time.sleep(0.025)	
 
 		except KeyboardInterrupt:
-			landing(board)
+#			landing(board)
 			board.disarm()          #disarm the board
 			board.disable_arm()
-		
-		landing(board)
-		board.disarm()
-		board.disable_arm()
+	return	
 
 def testswitch():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -100,10 +119,10 @@ def testswitch():
             print(converted_data[1])
 
 if __name__ == "__main__":
-	t1 = thread.Thread(target=control)
+#	t1 = thread.Thread(target=control)
 	#t2 = thread.Thread(target=video.videoserver)
-	t1.start()
+#	t1.start()
 	#t2.start()
-	t1.join()
+#	t1.join()
 	#t2.join()
-
+	control()
